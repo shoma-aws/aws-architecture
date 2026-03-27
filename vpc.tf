a2db7cd99786036ee31849e2b5e497cb47c7b18d
shoma-aws <shomashoma1013@gmail.com>
2026-03-27 11:39:45 +0900

    add initial VPC configuration

---
+++ vpc.tf
@@ -0,0 +1,12 @@
+'VPCを10.0.0.0～10.0.255.255の範囲で作成
+resource "aws_vpc" "main" {
+	cidr_block = "10.0.0.0/16"
+	enable_dns_support = true
+	enable_dns_hostnames = true
+
+	tags = {
+		Name = "sample-vpc"
+		Enviroment = "dev"
+		ManagedBy = "terraform"
+	}
+}
