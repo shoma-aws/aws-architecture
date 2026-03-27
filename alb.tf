b77dcb66cab50b03fa1e1c68298747dda1d6b23b
shoma-aws <shomashoma1013@gmail.com>
2026-03-27 15:11:53 +0900

    Application Load Balancer と Target Group を追加

---
+++ alb.tf
@@ -0,0 +1,48 @@
+#ALB用のセキュリティグループを作成
+resource "aws_security_group" "alb"
+	name = "alb-sg"
+	vpc_id = aws_vpc.main.id
+	ingress {
+		from_port = 80
+		to_port = 80
+		protocol = "tcp
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	} 		
+	tags = {
+		Name = "alb-sg"
+	}
+}
+
+#ALBを作成
+resource "aws_alb" "app" {
+	name = "app-alb"
+	load_balancer_type = "application"
+	internal = false
+	security_groups = [aws_security_group.alb.id]
+	subnets = [
+		aws_subnet.public_a.id,aws_subnet.public_c.id
+		]
+	tags = {
+		Name = "app-alb"
+	}
+}
+
+#TargetGroupを作成
+resource "aws_lb_target_group" "app" {
+	name = "app-tg"
+	port = 80
+	protcol = "HTTP"
+	vpc_id = aws_vpc.main.id
+	health_check {
+		path = "/"          #とりあえず"/"で指定。ヘルスチェック用のサーバーがあればそれに変更
+		}
+	tags = {
+		Name = "app-tg"
+	}
+}
