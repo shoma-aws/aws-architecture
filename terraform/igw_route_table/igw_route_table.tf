fa0264220b8aead8f554a276654fb0715968be63
shoma-aws <shomashoma1013@gmail.com>
2026-04-04 14:23:23 +0900

    ディレクトリを変更

---
+++ terraform/igw_route_table/igw_route_table.tf
@@ -0,0 +1,29 @@
+#InternetGatewayを作成
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+#PublicRouteTableを作成
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cider_block = "0.0.0.0/0"
+    gateway_id  = aws_internet_gateway.main.id
+  }
+  tags = {
+    Name = "pubric-rt"
+  }
+}
+
+#RouteTableをPublicSubnetに紐づけ
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "public_c" {
+  subnet_id      = aws_subnet.public_c.id
+  route_table_id = aws_route_table.public.id
+}
