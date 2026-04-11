00d07919d86ca75eae6f707db2a7afb5adf5321c
shoma-aws <shomashoma1013@gmail.com>
2026-04-11 12:24:41 +0900

    誤字を修正

--- terraform/asg/asg_ec2.tf
+++ terraform/asg/asg_ec2.tf
@@ -4,17 +4,17 @@ resource "aws_security_group" "ec2" {
   vpc_id = aws_vpc.main.id
   #ALBからのHTTP通信のみ許可
   ingress {
-    from_port      = 80
-    to_port        = 80
-    protocol       = "tcp"
-    security_group = [aws_security_group.alb.id]
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
   }
   #外向き通信は許可
   egress {
-    from_port  = 0
-    to_port    = 0
-    protocol   = "-1"
-    cidr_block = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
     Name = "ec2-sg"
